842c0c5d1657a2b8df2ae26d1da091cac3dcb96a
angel <angeeliaw@gmail.com>
2022-12-05 22:13:38 +0700

    update not-found

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,34 +1,24 @@
 import React from 'react'
-import '../notfound.css'
 
 // Modifikasi halaman berikut untuk menjadi halaman 404 yang menarik!
 const NotFound = () => {
   return (
-    <div>
-      <section class="page_404">
-        <div class="container">
-          <div class="row">	
-          <div class="col-sm-12 ">
-          <div class="col-sm-10 col-sm-offset-1  text-center">
-          <div class="four_zero_four_bg">
-            <h1 class="text-center ">404</h1> 
-          </div>
-          
-          <div class="contant_box_404">
-          <h3 class="h2">
-          Look like you're lost
-          </h3>
-          
-          <p>the page you are looking for not avaible!</p>
-          
-        </div>
-          </div>
-          </div>
-          </div>
-        </div>
-      </section>
+    <body>
+  <section class="notFound">
+    <div class="img">
+      <img src="https://assets.codepen.io/5647096/backToTheHomepage.png" alt="Back to the Homepage" />
+      <img src="https://assets.codepen.io/5647096/Delorean.png" alt="El Delorean, El Doc y Marti McFly" />
     </div>
-  )
+    <div class="text">
+      <h1>404</h1>
+      <h2>PAGE NOT FOUND</h2>
+      <h3>BACK TO HOME?</h3>
+      <a href="#" class="yes">YES</a>
+      <a href="https://www.youtube.com/watch?v=G3AfIvJBcGo">NO</a>
+    </div>
+  </section>
+</body>
+)
 }
 
 export default NotFound
